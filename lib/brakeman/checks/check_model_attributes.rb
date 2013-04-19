74130308db16b05610750e047d59fbfd53d84b67
Noah Davis <noahd1@yahoo.com>
2013-04-19 00:58:14 -0400

    Revert first attempt at fix of warning_code
    Using mainline commit instead.

--- lib/brakeman/checks/check_model_attributes.rb
+++ lib/brakeman/checks/check_model_attributes.rb
@@ -3,7 +3,7 @@ require 'brakeman/checks/base_check'
 #Check if mass assignment is used with models
 #which inherit from ActiveRecord::Base.
 #
-#If tracker.options[:collapse_mass_assignment] is +true+ (default), all models
+#If tracker.options[:collapse_mass_assignment] is +true+ (default), all models 
 #which do not use attr_accessible will be reported in a single warning
 class Brakeman::CheckModelAttributes < Brakeman::BaseCheck
   Brakeman::Checks.add self
@@ -57,7 +57,6 @@ class Brakeman::CheckModelAttributes < Brakeman::BaseCheck
           warn :model => name,
             :file => model[:file],
             :warning_type => "Attribute Restriction",
-            :warning_code => :no_attr_accessible,
             :message => "Mass assignment is not restricted using attr_accessible",
             :confidence => CONFIDENCE[:high]
         elsif not tracker.options[:ignore_attr_protected]
